1a363a96108c0e273a2d9d21f62e2ac4a796dc5e
15838028035 <15838028035@163.com>
2017-02-18 16:59:58 +0800

    StringUtil.java add new method

--- src/main/java/com/lj/app/core/common/util/StringUtil.java
+++ src/main/java/com/lj/app/core/common/util/StringUtil.java
@@ -1,5 +1,10 @@
 package com.lj.app.core.common.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.math.BigDecimal;
@@ -1278,4 +1283,47 @@ public class StringUtil {
 		}
 		return "";
 	}
+	
+	 /**
+     * 对象转数组
+     * @param obj
+     * @return
+     */
+    public static byte[] object2Byte (Object obj) {
+        byte[] bytes = null;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+            oos.writeObject(obj);
+            oos.flush();
+            bytes = bos.toByteArray();
+            oos.close();
+            bos.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return bytes;
+    }
+
+
+    /**
+     * 数组转对象
+     * @param bytes
+     * @return
+     */
+    public static Object byte2Object (byte[] bytes) {
+        Object obj = null;
+        try {
+            ByteArrayInputStream bis = new ByteArrayInputStream (bytes);
+            ObjectInputStream ois = new ObjectInputStream(bis);
+            obj = ois.readObject();
+            ois.close();
+            bis.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        return obj;
+    }
 }
