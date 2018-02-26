57cb650691ed291c6e02409e95fa91a863df59fe
15838028035 <15838028035@163.com>
2018-02-26 16:06:05 +0800

    代码生成器优化,jframe支持修改数据库地址,帐号,密码

--- src/main/java/com/lj/app/core/common/generator/GeneratorMainFrame.java
+++ src/main/java/com/lj/app/core/common/generator/GeneratorMainFrame.java
@@ -28,6 +28,20 @@ public class GeneratorMainFrame extends JFrame implements ActionListener {
   private JLabel titleInformation;
 
   /**
+   * jdbc.url
+   */
+  private JLabel jdbcUrl;
+  
+  /**
+   * jdbc.username
+   */
+  private JLabel jdbcUsername;
+  /**
+   * jdbc.password
+   */
+  private JLabel jdbcPassword;
+  
+  /**
    * 模板目录
    */
   private JLabel templateDir;
@@ -53,6 +67,19 @@ public class GeneratorMainFrame extends JFrame implements ActionListener {
   private JLabel table;
 
   /**
+   * jdbcUrl
+   */
+  private JTextField jdbcUrlTextFiled;
+  /**
+   * jdbcUsername
+   */
+  private JTextField jdbcUsernameTextFiled;
+  /**
+   * jdbcPassword
+   */
+  private JTextField jdbcPasswordTextFiled;
+  
+  /**
    * 模板目录
    */
   private JTextField templateDirTextFiled;
@@ -76,6 +103,20 @@ public class GeneratorMainFrame extends JFrame implements ActionListener {
    * 表
    */
   private JTextField tableTextField;
+  
+  /**
+   * jdbc.Url
+   */
+  private String jdbcUrlProp;
+  
+  /**
+   * jdbc.username
+   */
+  private String jdbcUsernameProp;
+  /**
+   * jdbc.password
+   */
+  private String jdbcPasswordProp;
 
   /**
    * 模板目录
@@ -148,6 +189,9 @@ public class GeneratorMainFrame extends JFrame implements ActionListener {
     basepackageProp = GeneratorProperties.getProperty("basepackage");
     outRootProp = GeneratorProperties.getProperty("outRoot");
     tableProp = GeneratorProperties.getProperty("TABLE_NAME");
+    jdbcUrlProp = GeneratorProperties.getProperty("jdbc.url");
+    jdbcUsernameProp = GeneratorProperties.getProperty("jdbc.username");
+    jdbcPasswordProp = GeneratorProperties.getProperty("jdbc.password");
   }
 
   /**
@@ -158,53 +202,72 @@ public class GeneratorMainFrame extends JFrame implements ActionListener {
     titleInformation = new JLabel("代码生成器");
 
     add(g, c, titleInformation, 0, 0, 1, 1);
+    
+    jdbcUrl = new JLabel("JdbcUrl：");
+    add(g, c, jdbcUrl, 0, 1, 1, 1);
+    jdbcUrlTextFiled = new JTextField(50);
+    jdbcUrlTextFiled.setText(jdbcUrlProp);
+    add(g, c, jdbcUrlTextFiled, 1, 1, 1, 1);
+    
+    jdbcUsername = new JLabel("用户名：");
+    add(g, c, jdbcUsername, 0, 2, 1, 1);
+    jdbcUsernameTextFiled = new JTextField(50);
+    jdbcUsernameTextFiled.setText(jdbcUsernameProp);
+    add(g, c, jdbcUsernameTextFiled, 1, 2, 1, 1);
+    
+    jdbcPassword = new JLabel("密码：");
+    add(g, c, jdbcPassword, 0, 3, 1, 1);
+    jdbcPasswordTextFiled = new JTextField(50);
+    jdbcPasswordTextFiled.setText(jdbcPasswordProp);
+    add(g, c, jdbcPasswordTextFiled, 1, 3, 1, 1);
+    
 
     templateDir = new JLabel("模板目录：");
 
-    add(g, c, templateDir, 0, 1, 1, 1);
+    add(g, c, templateDir, 0, 4, 1, 1);
 
     templateDirTextFiled = new JTextField(50);
     templateDirTextFiled.setText(templateDirProp);
 
-    add(g, c, templateDirTextFiled, 1, 1, 1, 1);
+    add(g, c, templateDirTextFiled, 1, 4, 1, 1);
 
     schema = new JLabel("schema：");
-    add(g, c, schema, 0, 2, 1, 1);
+    add(g, c, schema, 0, 5, 1, 1);
 
     schemaTextField = new JTextField(50);
     schemaTextField.setText(schemaProp);
-    add(g, c, schemaTextField, 1, 2, 2, 1);
+    add(g, c, schemaTextField, 1, 5, 2, 1);
 
     basepackage = new JLabel("包名：");
-    add(g, c, basepackage, 0, 3, 1, 1);
+    add(g, c, basepackage, 0, 6, 1, 1);
 
     basepackageTextField = new JTextField(50);
     basepackageTextField.setText(basepackageProp);
-    add(g, c, basepackageTextField, 1, 3, 2, 1);
+    add(g, c, basepackageTextField, 1, 6, 2, 1);
 
     outRoot = new JLabel("输出目录：");
-    add(g, c, outRoot, 0, 4, 1, 1);
+    add(g, c, outRoot, 0, 7, 1, 1);
 
     outRootTextField = new JTextField(50);
     outRootTextField.setText(outRootProp);
-    add(g, c, outRootTextField, 1, 4, 2, 1);
+    add(g, c, outRootTextField, 1, 7, 2, 1);
 
     table = new JLabel("表名：");
-    add(g, c, table, 0, 5, 1, 1);
+    add(g, c, table, 0, 8, 1, 1);
 
     tableTextField = new JTextField(50);
     tableTextField.setText(tableProp);
-    add(g, c, tableTextField, 1, 5, 2, 1);
+    add(g, c, tableTextField, 1, 8, 2, 1);
 
     submit = new JButton("生成");
 
     c.insets = new Insets(8, 10, 4, 0);
 
-    add(g, c, submit, 1, 6, 1, 1);
+    add(g, c, submit, 1, 9, 1, 1);
 
     result = new JTextArea(15, 50);
 
-    add(g, c, result, 0, 7, 3, 4);
+    add(g, c, result, 0, 10, 3, 4);
 
   }
 
@@ -237,6 +300,10 @@ public class GeneratorMainFrame extends JFrame implements ActionListener {
     String basepackageStr = basepackageTextField.getText();
     String outRootStr = outRootTextField.getText();
     String tableStr = tableTextField.getText();
+    
+    String jdbcUrl = jdbcUrlTextFiled.getText();
+    String jdbcUsername = jdbcUsernameTextFiled.getText();
+    String jdbcPasswordText = jdbcPasswordTextFiled.getText();
 
     System.out.println("templateDirStr:" + templateDirStr);
 
@@ -244,6 +311,10 @@ public class GeneratorMainFrame extends JFrame implements ActionListener {
 
     try {
 
+      GeneratorProperties.setProperty("jdbc.url", jdbcUrl);
+      GeneratorProperties.setProperty("jdbc.username", jdbcUsername);
+      GeneratorProperties.setProperty("jdbc.password", jdbcPasswordText);
+      
       GeneratorProperties.setProperty("basepackage", basepackageStr);
       GeneratorProperties.setProperty("basepackage_dir",
           GeneratorProperties.getProperty("basepackage").replace(".", "/"));
